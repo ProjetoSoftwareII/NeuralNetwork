8e7cf7e9738d9d34f8a3153be074541ec25501ba
Henrique-Liesenfeld-Krever <henriqueliesenfeld@hotmail.com>
2022-12-27 14:04:38 -0300

    Rede treinada, realizar o teste

--- reconFacial.py
+++ reconFacial.py
@@ -204,7 +204,8 @@ if not teste:
 else:
   #loss de 54% de acerto 'lt: 304.73346 lv: 97.06043 epoch:40.h5'
   #loss de  52   de acerto 'lt: 87.30312 lv: 30.92616 epoch:45.h5' T 1.29
-  model = tf.keras.models.load_model('/usr/app/src/dataset/treino/lt: 120.85935 lv: 41.15373 epoch:40.h5')
+  #loss de 53% de acerto 'lt: 120.85935 lv: 41.15373 epoch:40.h5'
+  model = tf.keras.models.load_model('/usr/app/src/dataset/treino/rede_treinada.h5')
   
   T = 1.63
   #t funcionando 0.98
